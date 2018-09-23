d3538a841b718fb5940dec27dbaa6a54d35ecefc
Sanne Peters <sanne@sanstream.nl>
2018-09-23 18:29:59 +0200

    wip: reworing data mapping based on country codes and language codes.

--- research/refined-data/parse-cia-lang.js
+++ research/refined-data/parse-cia-lang.js
@@ -1,5 +1,44 @@
 const fs = require('fs')
 
+var output = {}
+var countryLookup = {}
+var langLookUp = {}
+
+const countryCodesCSV = fs.readFileSync('../country-codes.csv', 'utf8')
+countryCodesCSV.split('\n').forEach(el => {
+  const items = el.split(',')
+  output[items[1]] = {
+    countryName: items[0],
+  }
+  countryLookup[items[0]] = {
+    code: items[1],
+  }
+})
+
+const langCodesCSV = fs.readFileSync('../language-codes.csv', 'utf8')
+langCodesCSV.split('\n').forEach(el => {
+  const items = el.split(',')
+  langLookUp[items[4]] = {
+    languageName: items[1],
+  }
+})
+
+const populations = fs.readFileSync('world-populations.csv', 'utf8').split('\n')
+populations.forEach(function (element) {
+  let pairing = element.split(',')
+  if (pairing.length === 2) {
+    const country = pairing[0].replace(/^[\s]+/g, '').replace(/\s[.\d]+$/, '')
+    const count = parseInt(pairing[1].replace(/[\s]+/g, '').match(/[\d]+$/), 10) * 1000
+    if (countryLookup[country]) {
+      output[countryLookup[country].code]['populationCount'] = count
+    }
+  }
+})
+
+console.log(output)
+
+fs.writeFile('world-languages-2.json', JSON.stringify(output), 'utf8', () => {})
+
 fs.readFile('cia-language-census.txt', 'utf8', function (err, data) {
   if (err) {
     console.log(err)
@@ -33,27 +72,5 @@ fs.readFile('cia-language-census.txt', 'utf8', function (err, data) {
         objectified[keyTracker]['languages'][language] = parseFloat(percentage[0])
       }
     }
-  })
-
-  fs.readFile('world-populations.csv', 'utf8', function (err, data) {
-    if (err) {
-      console.log(err)
-    }
-    let byLines = data.split('\n')
-    byLines.forEach(function (element) {
-      let pairing = element.split(',')
-      if (pairing.length === 2) {
-        const country = pairing[0].replace(/^[\s]+/g, '').replace(/\s[.\d]+$/, '')
-        const count = parseInt(pairing[1].replace(/[\s]+/g, '').match(/[\d]+$/), 10) * 1000
-        if (!objectified[country]) {
-          // console.log(country)
-        } else {
-          objectified[country]['populationCount'] = count
-        }
-      }
-    })
-
-    // console.log(objectified)
-    fs.writeFile('world-languages.json', JSON.stringify(objectified), 'utf8', function () {})
   })
 })
